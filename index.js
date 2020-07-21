3b15e8603a776b0703dfc9efd8651e98a8284e07
cortezd334 <cortezd334@gmail.com>
2020-07-21 16:34:26 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+const submitData = (user_name, user_email) => {
+
+    let body = document.querySelector('body')
+
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify({name: user_name, email: user_email})
+    })
+    .then(res => res.json())
+    .then(json => {
+        let div = document.createElement('div')
+        div.innerHTML = json.id
+        body.appendChild(div)    
+    })
+    .catch(error => {
+        let div = document.createElement('div')
+        div.innerHTML = error.message
+        body.appendChild(div)
+    })
+
+}
+submitData('user_name', 'user_email')
