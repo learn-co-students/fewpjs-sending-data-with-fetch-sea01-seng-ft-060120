366511410cafc6d639a3ab85efb2b609029947e5
jlorenc1 <joshua.lorenc@icloud.com>
2020-07-22 10:50:18 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+
+function submitData(name, email) {
+    const url = 'http://localhost:3000/users';
+    return fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(object) {
+            document.body.innerHTML = object['id']
+        })
+        .catch(function(error) {
+            document.body.innerHTML = error.message
+        });
+} 
